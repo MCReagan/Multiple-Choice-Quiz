32f2afd54994463c9cf0f550800eee3c763a8ef6
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-08 00:17:17 -0500

    added timer to page on start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Need to create function to start game when game is pressed
 var startButton = document.querySelector("#startButton");
+var timeEl = document.getElementById("time");
+
 var buttonAttributes = {
     style: 'background-color: var(--purple); color: white;'
 }
@@ -18,8 +20,35 @@ buttonChange(startButton, buttonAttributes)
 
 startButton.addEventListener("click", function () {
     startGame();
+    startTimer();
 });
 
+function startTimer() {
+    var timeLeft = 31;
+
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 30) {
+            timeEl.textContent = timeLeft + ' seconds';
+            timeLeft--;
+        } else if (timeLeft > 10) {
+            timeEl.setAttribute("style", "color: yellow");
+            timeEl.textContent = timeLeft + ' seconds';
+            timeLeft--;
+        } else if (timeLeft > 1) {
+            timeEl.setAttribute("style", "color: red");
+            timeEl.textContent = timeLeft + ' seconds!';
+            timeLeft--;
+        } else if (timeLeft == 1) {
+            timeEl.textContent = timeLeft + ' second!';
+            timeLeft--;
+            console.log(timeLeft);
+        } else {
+            timeEl.textContent = 'You lost!';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+    timeEl.textContent = '';
+};
 
 function startGame() {
     

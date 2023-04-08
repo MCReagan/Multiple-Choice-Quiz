6922bd90adebeb62f58a4b78b8e02ef0664b2562
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-07 23:10:02 -0500

    added styling to button on hover

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
 // Need to create function to start game when game is pressed
-var startButton = document.querySelector("#start");
-var startHover = document.querySelector("#start");
+var startButton = document.querySelector("#startButton");
+var buttonAttributes = {
+    style: 'background-color: var(--green); color: white;'
+}
 
-startHover.addEventListener("hover", function() {
-    var changeButton = document.getElementById(startButton);
-    console.log("ding");
-    changeButton.style.backgroundColor = "purple";
+startButton.addEventListener("mouseover", function () {
+buttonChange(startButton, buttonAttributes)
+    function buttonChange(element, buttonAttributes) {
+        Object.keys(buttonAttributes).forEach(attribute => {
+            element.setAttribute(attribute, buttonAttributes[attribute]);
+        });
+    }
+    startButton.addEventListener("mouseout", function () {
+        startButton.setAttribute("style", "background-color: ");
+    })
 });
 
-startButton.addEventListener("click", function() {
-startGame();
+startButton.addEventListener("click", function () {
+    startGame();
+    return;
 });
 
 
 function startGame() {
-return;
+    return;
 };
 
 // Need to create function to replace screen content as game is played

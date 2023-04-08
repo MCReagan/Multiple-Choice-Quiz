50ca4d455571605c0098b23716f8ae72407f74a4
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-07 21:52:40 -0500

    added base styling of page and set some margins, added placeholders for different features

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Need to create function to start game when game is pressed
+var startButton = document.querySelector('#start');
+
+startButton.addEventListener("click", function() {
+startGame();
+});
+
+
+function startGame() {
+
+}
+
+// Need to create function to replace screen content as game is played
+
